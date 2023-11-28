64b77bfa65a0ba12d3222d06b6c4add4c7085812
Victor <victormakanda254@gmail.com>
2023-11-28 11:51:02 +0300

    First commit

--- lib/person.py
+++ lib/person.py
@@ -4,4 +4,9 @@ class Person:
     # Class body goes here
 
     #Instance method definition
+    def talk():
+        print("Hello World!")
+    
+    def walk():
+        print("The person is walking.")
     pass
